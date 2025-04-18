4a55d264ee84f4885715d0994823519804b497bd
lhernandez66@ivytech.edu <lhernandez66@ivytech.edu>
2025-04-18 17:24:34 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/SDEV_255_Final_Project_Group2/',  // use the repo name as the base path
+  base: 'https://lhernandez66.github.io/SDEV_255_Final_Project_Group2/',  // use the repo name as the base path
   plugins: [react()],
 });
